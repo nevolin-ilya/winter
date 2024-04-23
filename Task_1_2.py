540eff85c03dff2771827f7bd4599e19d438a6ce
NevolinIS <lianevolin@gmail.com>
2024-04-23 16:28:47 +0300

    Task_1_2_

---
+++ Task_1_2.py
@@ -0,0 +1,13 @@
+x = int(input())
+
+y = int(input())
+
+z1 = (x + y)
+z2 = (x - y)
+z3 = (x*y)
+z4 = (x/y)
+z5 = (x//y)
+
+list1 = [z1, z2, z3, z4, z5]
+Max = max(list1)
+print("Наибольшее число", Max)
