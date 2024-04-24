73e2bba73d767ee118c0a76517ecccfd222ae2e9
NevolinIS <lianevolin@gmail.com>
2024-04-24 10:05:56 +0300

    Task_1_2

--- Task_1_2.py
+++ Task_1_2.py
@@ -9,5 +9,37 @@ z4 = (x/y)
 z5 = (x//y)
 
 list1 = [z1, z2, z3, z4, z5]
-Max = max(list1)
+Max1 = max(list1)
 print("Наибольшее число", Max)
+
+#второй вариант
+
+x = int(input())
+
+y = int(input())
+
+z1 = (x + y)
+z2 = (x - y)
+z3 = (x*y)
+z4 = (x/y)
+z5 = (x//y)
+
+if z1 > z2:
+    max = z1
+else:
+    max = z2
+
+if z3 > max:
+    max = z3
+else:
+    max = max
+if z4 > max:
+    max = z4
+else:
+    max = max
+if z5 > max:
+    max = z5
+else:
+    max = max
+
+print(max)
