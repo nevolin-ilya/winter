848581e3cd5a1ac457c2f6372202c8357219ad32
NevolinIS <lianevolin@gmail.com>
2024-05-22 16:28:12 +0300

    Task_7_2

--- Task_7_2.py
+++ Task_7_2.py
@@ -1,8 +1,10 @@
 def max_sort(x):
     lst = []
-    for i in range(len(x)):
-        for j in range(len(x[i])):
+    for i in x:
+        for j in i:
             lst.append(j)
+            lst.sort()
+    return lst[-3:]
 
 x = [[7,2,5], [6,3,9]]
 print(max_sort(x))
