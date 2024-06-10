d02e1466dd73523cea8a529cf783e5873358bab3
NevolinIS <lianevolin@gmail.com>
2024-06-10 13:17:21 +0300

    Task_15_2

--- Task_15_2.py
+++ Task_15_2.py
@@ -3,8 +3,8 @@ import re
 
 def nambers(x):
 
-    pattern = r"\w\d\d\d\w\w78" or r"\w\d\d\d\w\w178"
-    match = re.match(pattern, x)
-    return bool(match)
+    pattern = r"\w\d\d\d\w\w78|\w\d\d\d\w\w178"
+    match1 = re.findall(pattern, x)
+    return match1
 
 print(nambers(input()))
