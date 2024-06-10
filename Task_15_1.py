1c4e1a0e6dd828ca75630344f6ebf530b0d27f0f
NevolinIS <lianevolin@gmail.com>
2024-06-10 13:17:02 +0300

    Task_15_1

--- Task_15_1.py
+++ Task_15_1.py
@@ -1,14 +1,13 @@
-dct = {1:1, 2:2, 3:{2:22, 3:{1:111, 2:222, 3:{0:1111, 1:2222, 2:3333}}, 1:11}, 2:11}
-def f(dct):
+dct = {1:1, 2:2, 3:{2:22, 3:{1:111, 2:222, 1:{0:1111, 1:2222, 2:3333}}, 1:11}, 2:11}
+def f(dct, x):
     res = []
     for k, v in dct.items():
-        print(k)
-        if k == 1:
+        if k == x:
             res.append(v)
         elif type(k) == str:
             pass
         elif type(v) == dict:
-            res.extend(f(v))
+            res.extend(f(v, x))
     print(res)
     return res
-print(f(dct))
+print(f(dct, 1))
