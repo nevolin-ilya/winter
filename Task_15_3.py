c1ce13ac5a42f057d59939b333b22d1a74e16d7a
NevolinIS <lianevolin@gmail.com>
2024-06-10 13:31:45 +0300

    Task_15_3

--- Task_15_3.py
+++ Task_15_3.py
@@ -2,8 +2,8 @@
 import re
 
 def nambers(x):
-    #s = x.split(" ")
-    pattern = r"\+7(812)\d\d\d-\d\d\d\d|\+7(812)\d\d\d-\d\d-\d\d|\+7(921)\d\d\d-\d\d\d\d|\+7(921)\d\d\d-\d\d-\d\d"
+
+    pattern = r"\+7\(812\)\d\d\d-\d\d\d\d|\+7\(812\)\d\d\d-\d\d-\d\d|\+7\(921\)\d\d\d-\d\d\d\d|\+7\(921\)\d\d\d-\d\d-\d\d"
     match1 = re.findall(pattern, x)
     return match1
 
