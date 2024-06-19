c96366747036c4d882207b1cbafe4bcdfef044d3
NevolinIS <lianevolin@gmail.com>
2024-06-19 16:29:14 +0300

    Task_18

--- Task_18.py
+++ Task_18.py
@@ -2,30 +2,36 @@ class Data:
     def __init__(self, task): self.task = task
 
 class Teacher:
-    def __init__(self, result):
-        self.work = 0
-        self.result = result
+    def __init__(self):
+        self.task = 0
+        self.result = []
         self.solution = []
     def send_task(self, task, student):
-        student.take(task)
-        self.work += 1
+        student.take_task(task)
+        self.task += 1
     def take_solution(self, solution):
         self.solution.append(solution)
     def send_result(self, result, student):
         student.take_result(result)
         result += 1
 class Student:
-    def __init__(self, name, solution):
+    def __init__(self, name):
         self.name = name
         self.knowledge = []
         self.zachet = []
-        self.solution = solution
+        self.solution = []
     def take_task(self, task):
         self.knowledge.append(task)
     def send_solution(self, solution, teacher):
-        teacher.take(solution)
-        self.solution += 1
+        teacher.take_solution(solution)
+        self.solution.append(solution)
     def take_result(self, result):
         self.zachet.append(result)
 
+t = Teacher()
+p = Student("Ivan")
+d = Data([1])
+t.send_result(d.task[0], p)
+print(t.task)
+print(p.zachet)
 
