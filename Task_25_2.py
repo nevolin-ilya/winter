d6b14fc09009c640ccf32dd9a8938bbe0cd62a91
NevolinIS <lianevolin@gmail.com>
2024-07-05 09:45:27 +0300

    Task_25_2

---
+++ Task_25_2.py
@@ -0,0 +1,11 @@
+
+def palindrome(s):
+    if len(s) < 1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return palindrome(s[1:-1])
+        else:
+            return False
+print(palindrome(input()))
+
